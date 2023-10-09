140222d398198f9bbf367b26f41b90cd8923a916
Bruno Fukumori <brunofukumori@gmail.com>
2023-10-09 16:48:34 -0300

    aula 1

---
+++ src/test/java/br/com/bfukumori/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bfukumori.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
